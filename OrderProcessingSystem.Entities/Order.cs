1e2534ef9f2a6f5b240846f333a33e5093cff6be
hajusana <sana.akbar@krazy-tech.com>
2025-01-27 17:34:11 +0530

    Added Entity Models

---
+++ OrderProcessingSystem.Entities/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderProcessingSystem.Entities
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Products.Sum(product => product.Price);
+            }
+        }
+    }
+}
---
+++ OrderProcessingSystem.Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderProcessingSystem.Entities
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
