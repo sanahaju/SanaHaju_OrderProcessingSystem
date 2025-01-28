009cf4068693ae7fc24b8c67be076a6fbe412ea1
hajusana <sana.akbar@krazy-tech.com>
2025-01-28 10:27:01 +0530

    Added Connectionstring and DI Configurtaions

--- OrderProcessingSystem/Program.cs
+++ OrderProcessingSystem/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using OrderProcessingSystem.BusinessLayer.Interfaces;
+using OrderProcessingSystem.BusinessLayer.Repository;
+using OrderProcessingSystem.BusinessLayer.Services;
+using OrderProcessingSystem.DataLayer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IOrderProcessingRepository, OrderProcessingRepository>();
+builder.Services.AddScoped<IOrderProcessingService, OrderProcessingService>();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
